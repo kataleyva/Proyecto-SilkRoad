9704b0564212eb2e3daca716cc46a3f9b0fbbefb
Michruiz28 <michruiz2827@gmail.com>
2025-10-09 16:56:48 -0500

    Test SilkRoadTest Terminados

--- silkroad/SilkRoad.java
+++ silkroad/SilkRoad.java
@@ -375,7 +375,7 @@ public class SilkRoad {
      * Permite identificar el robot con mayor ganancias
      * @return void
      */
-    private void getRobotHighestProfits() {
+    public void getRobotHighestProfits() {
         if (robots.isEmpty()) return; 
     
         ArrayList<Integer> profits = new ArrayList<>();
